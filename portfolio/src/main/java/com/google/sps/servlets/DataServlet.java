42ef4501e0b8204f474027d8dbe33324f92d72fa
Nitish Bharti <nitishbharti235@gmail.com>
2020-07-15 23:04:42 +0530

    change in response message

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -27,6 +27,6 @@ public class DataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("text/html;");
-    response.getWriter().println("Hello Nitish! \t Just wanted to tell you, Ur Special!!");
+    response.getWriter().println("Hello Nitish. \t Just wanted to tell you, Ur Special!!");
   }
 }
